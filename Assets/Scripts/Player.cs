61be4b5f135bb6edb5b3378e27a01c7dee1ccc49
Gabriel Bandeira <gabrielvbdev@gmail.com>
2025-11-04 19:40:07 -0300

    [FEAT] Adicionando animação ao player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private bool isWalking;
+
     // Update is called once per frame
     private void Update()
     {
@@ -14,6 +16,13 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         transform.forward = Vector3.Slerp (transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Player player;
+
+    private Animator animator;
+
+    private const string IS_WALKING = "IsWalking";
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
